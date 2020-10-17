1b58e2e8b91b2671df31f88e4b29317e3c89d0db
Jessica-Azcona <jessicaazconar@gmail.com>
2020-10-17 08:39:41 -0500

    'base'

---
+++ trash/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {AppRoutingModule} from './app-routing.module';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './main-components/header/header.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { HomePComponent } from './main-components/home-p/home-p.component';
+import { MenuComponent } from './main-components/menu/menu.component';
+
+import { registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es-MX';
+import localeEn from '@angular/common/locales/en';
+
+import { ModulosSecundariosModule } from './modules/modulos-secundarios/modulos-secundarios.module';
+import { ContenidoComponent } from './modules/modulos-secundarios/contenido/contenido.component';
+import { BarraComponent } from './modules/modulos-secundarios/barra/barra.component';
+import { ChartsModule} from  'ng2-charts';
+import { PageNotFoundComponent } from './main-components/page-not-found/page-not-found.component';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+registerLocaleData(localeEs,'es-MX');
+registerLocaleData(localeEn,'en');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePComponent,
+    MenuComponent,
+    BarraComponent, 
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    ModulosSecundariosModule,
+    AppRoutingModule,
+    FormsModule,
+    ChartsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ trash/src/app/modules/modulos-secundarios/modulos-secundarios.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+import { ContenidofComponent } from './contenidof/contenidof.component';
+import { ContenidoCComponent } from './contenido-c/contenido-c.component';
+import { ContactComponent } from './contact/contact.component';
+import { BajaComponent } from './baja/baja.component';
+import { IndexRoutingModule } from './index-routing-module';
+import { FormsModule } from '@angular/forms';
+import { BarraComponent } from './barra/barra.component';
+import { ChartsModule} from  'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { ContenidoComponent } from './contenido/contenido.component';
+
+@NgModule({
+  declarations: [LoginComponent, SigninComponent, RegistrarComponent, ContenidofComponent,ContenidoComponent, ContenidoCComponent, ContactComponent, BajaComponent],
+  imports: [
+    CommonModule,
+    IndexRoutingModule,
+    FormsModule,
+    ChartsModule,
+    BrowserModule
+  ]
+})
+export class ModulosSecundariosModule { }
